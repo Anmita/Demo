753397ca397571d1e39da7c10d9dee0f89c73333
Anmita <42768580+Anmita@users.noreply.github.com>
2022-07-27 15:19:32 +0530

    Update code.c

--- code.c
+++ code.c
@@ -3,24 +3,18 @@
 #include<ctype.h>
 
 int main() {
-   char c;
-   int lowercase_vowel, uppercase_vowel;
+   char ;
+   int capital,small;
    printf("Enter an alphabet: ");
-   scanf("%c", &c);
-
-   // evaluates to 1 if variable c is a lowercase vowel
+   scanf("%c", &l);
    lowercase_vowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
-
-   // evaluates to 1 if variable c is a uppercase vowel
    uppercase_vowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
-
-   // Show error message if c is not an alphabet
    if (!isalpha(c))
-      printf("Error! Non-alphabetic character.");
-   else if (lowercase_vowel || uppercase_vowel)
-      printf("%c is a vowel.", c);
+      printf("not a letter");
+   else if (capital|| small)
+      printf("%c is a vowel.", l);
    else
-      printf("%c is a consonant.", c);
+      printf("%c is a consonant.", l);
 
    return 0;
 }
